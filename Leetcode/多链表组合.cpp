0bdfcf3c61acd0afa95efc0d488bf5af2373ed8f
fafadfa <xugewidi@outlook.com>
2021-07-26 13:04:18 +0800

    多链表

--- Leetcode/多链表组合.cpp
+++ Leetcode/多链表组合.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<string>
 
 using namespace std;
 
@@ -11,94 +12,42 @@ struct ListNode
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
-
-#if 0
-
-class MHeap {
+class Solution {
 public:
-    MHeap(uint64_t k) {
-        _heap.reserve(k);
-    }
-
-    void insertx(ListNode* nodep) {
-        _heap.emplace_back(nodep);
-        uint64_t child = _heap.size() - 1;
-        while (child > 0) {
-            uint64_t pa = (child - 1) / 2;
-            child = pa * 2 + 1;
-            if (child + 1 < _heap.size())
-                child = _heap[child]->val <= _heap[child + 1]->val ? child : child + 1;
-            if (_heap[child]->val < _heap[pa]->val)
-                swap(_heap[child], _heap[pa]);
-            else break;
-            child = pa;
-        }
-    }
-
-    void pop() {
-        swap(_heap[0], _heap[_heap.size() - 1]);
-        _heap.pop_back();
-        uint64_t pa = 0, child = 1;
-        while (pa * 2 + 1 < _heap.size()) {
-            child = pa * 2 + 1;
-            if (child + 1 < _heap.size())
-                child = _heap[child]->val <= _heap[child + 1]->val ? child : child + 1;
-            if (_heap[child]->val < _heap[pa]->val)
-                swap(_heap[child], _heap[pa]);
-            else break;
-            pa = child;
-        }
-    }
-
-    ListNode* top() {
-        return _heap[0];
-    }
-
-    bool empty() {
-        return _heap.empty();
-    }
-private:
-    vector<ListNode*> _heap;
-};
-
-class Solution 
-{
-public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) 
+    bool checkValidString(string s)
     {
-        if (lists.empty()) return nullptr;
+        int countR = 0;
+        int countS = 0;
 
-        MHeap heap(lists.size());
-
-        for (auto& it : lists) 
+        for (auto ch : s)
         {
-            if (it)
-                heap.insertx(it);
+            if (ch == '(') countR++;
+            else if (ch == '*') countS++;
+            else
+            {
+                if (countR)
+                    countR--;
+                else if (countS)
+                    countS--;
+                else return false;
+            }
         }
 
-        ListNode* dummy = new ListNode(0), * p = dummy;
-
-        while (!heap.empty()) 
-        {
-            ListNode* temp = heap.top();
-            heap.pop();
-            p->next = temp;
-            if (temp->next)
-                heap.insertx(temp->next);
-            p = p->next;
-        }
-
-        return dummy->next;
+        if (countR <= countS || countR == 0)
+            return true;
+        return false;
     }
-};
 
-#endif
+
+};
 
 int main()
 {
-   
+    string str("(((((*(()((((*((**(((()()*)()()()*((((**)())*)*)))))))(())(()))())((*()()(((()((()*(())*(()**)()(())");
 
-    /*s.mergeKLists();*/
+    Solution s;
+    s.checkValidString(str);
+    
 
     return 0;
 }
